4dd81492a1d89577dcfbdc8d98e24942349a0665
dmlstudent2 <dmlstudent2@spyponders.com>
2017-01-13 04:43:18 -0500

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -3,10 +3,14 @@ document.write('Fri #1');
 var numberOfKittens = 5;
 var numberOfPuppies = 6;
 console.log(numberOfKittens + numberOfPuppies);
+
+// using arithmetic operators
 var num1 = 16;
-var num2 = 16;
-console.log("multiply: ");
+var num2 = 5;
+console.log(num1 + num2);
+console.log(num1 - num2);
 console.log(num1 * num2);
-console.log("divide: ");
 console.log(num1 / num2);
+console.log(-num1);
+console.log(num1 % num2);
 
