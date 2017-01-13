15c23d1fda476312d7c4dc14625e0773c6dfcbf9
dmlstudent2 <dmlstudent2@spyponders.com>
2017-01-13 04:43:48 -0500

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -1,5 +1,5 @@
 console.log('This message STILL DOES appears in the console.');
-document.write('Fri #1');
+document.write('Fri #2');
 var numberOfKittens = 5;
 var numberOfPuppies = 6;
 console.log(numberOfKittens + numberOfPuppies);
