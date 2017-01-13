85594a91e22e8d4ef1ddbaf1c4b622456ded585e
dmlstudent2 <dmlstudent2@spyponders.com>
2017-01-13 05:04:40 -0500

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -1,5 +1,5 @@
 console.log('This message STILL DOES appears in the console.');
-document.write('Fri #7');
+document.write('Fri #8');
 var numberOfKittens = 5;
 var numberOfPuppies = 6;
 console.log(numberOfKittens + numberOfPuppies);
@@ -21,3 +21,6 @@ console.log("I\'d like to use an apostrope.");
 // concatenation
 var fullName = kittensName + " McDougal"; 
 console.log(fullName);
+
+// test
+document.write("Next!");
