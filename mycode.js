1deafb1089add8a8b8b913cdbdf6ca5b341fe8fa
dmlstudent2 <dmlstudent2@spyponders.com>
2017-01-17 05:15:04 -0500

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -1,5 +1,5 @@
 console.log('This message STILL DOES appears in the console.');
-document.write('<p>Fri #34</p>');
+document.write('<p>Fri #35</p>');
 /* var numberOfKittens = 5;
 var numberOfPuppies = 6;
 console.log(numberOfKittens + numberOfPuppies);
@@ -145,4 +145,4 @@ for ( i = 0; i < loveName.length; i++ ) {
  console.log(loveName[i]);
 }
 
-console.log("Jonas contains J? " + loveName.includes("J"));
+console.log("Jonas contains j? " + loveName.toLowerCase().includes("j"));
